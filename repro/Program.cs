6a4e37c8559ac39d892248be7f972644979aa425
mrzax <mrzax@DESKTOP-DPSBLMT>
2023-02-03 21:36:13 +0400

    Add project files.

---
+++ repro/Program.cs
@@ -0,0 +1,26 @@
+﻿using MemoryModule;
+using System.Runtime.InteropServices;
+
+namespace repro
+{
+    internal class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int addNumberProc(int a, int b);
+        static void Main(string[] args)
+        {
+            var asmBytes = File.ReadAllBytes(@"C:\Users\mrzax\source\repos\MemoryModule.NET-repro\reprolib\bin\Release\net7.0\publish\win-x64\reprolib.dll");
+
+            var asm = NativeAssembly.Load(asmBytes);
+            Console.WriteLine("Successfully loaded library.");
+            var addNumberFunc = asm.GetDelegate<addNumberProc>("addNumber");
+            var rand = new Random();
+            int num1 = rand.Next(0, 20);
+            int num2 = rand.Next(0, 20);
+            var result = addNumberFunc(num1, num2);
+            Console.WriteLine($"{num1}+{num2}={result}");
+            asm.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ reprolib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace reprolib
+{
+    public class Reprolib
+    {
+        [UnmanagedCallersOnly(EntryPoint = "addNumber", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
